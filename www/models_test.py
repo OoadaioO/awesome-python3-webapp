62abb55ff900529ae1ec0068306794894f4f7159
阿呆 <750983498@qq.com>
2017-02-11 22:52:33 +0800

    modify

--- www/models_test.py
+++ www/models_test.py
@@ -3,14 +3,15 @@
 
 __author__="阿呆"
 
-import orm
+import orm,asyncio
 from models import User, Blog, Comment
 
+loop = asyncio.get_event_loop()
 
-def test():
-    yield from orm.create_pool(user='www-data', password='www-data', db='awesome')
+async def test():
+    await orm.create_pool(loop=loop,user='www-data', password='www-data', db='awesome')
     u = User(name='Test', email='test@example.com', password='1234567890', image='about:blank')
-    yield from u.save()
+    await u.save()
 
-for x in test():
-    pass
+loop.run_until_complete(test())
+loop.close()
